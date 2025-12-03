47fb97fac844ed84394a35b93d447f8dadfef445
shawn <shawnbutton@gmail.com>
2025-12-02 19:19:06 -0500

    add kata framework

---
+++ php/src/Calculator.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Advent;
+
+class Calculator
+{
+    public function add(int $a, int $b): int
+    {
+        return $a + $b;
+    }
+
+    public function subtract(int $a, int $b): int
+    {
+        return $a - $b;
+    }
+
+    public function multiply(int $a, int $b): int
+    {
+        return $a * $b;
+    }
+
+    public function divide(int $a, int $b): float
+    {
+        if ($b === 0) {
+            throw new \InvalidArgumentException('Cannot divide by zero');
+        }
+        return $a / $b;
+    }
+}
